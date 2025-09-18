db0228e660081d5dccf28aee5c8c5abd6c4d616f
Jeremie-Baguma <jeremiebaguma54@gmail.com>
2025-09-18 10:27:57 +0200

    update the file

--- Hello.java
+++ Hello.java
@@ -1,5 +1,40 @@
-public class Main {
+class Car {
+    private String make;
+    private String model;
+    private int year;
+
+    // Constructor
+    public Car(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    // Getter for make
+    public String getMake() {
+        return make;
+    }
+
+    // Getter for model
+    public String getModel() {
+        return model;
+    }
+
+    // Getter for year
+    public int getYear() {
+        return year;
+    }
+
+    // Method to display car info
+    public void displayInfo() {
+        System.out.println("Car: " + year + " " + make + " " + model);
+    }
+}
+
+public class Hello {
     public static void main(String[] args) {
+        Car myCar = new Car("Toyota", "Corolla", 2020);
+        myCar.displayInfo();
         System.out.println("Hello world!");
     }
 }
